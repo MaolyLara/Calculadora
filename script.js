af152f3f5333e86c234c60c9ec6fe358d70d43d4
MaolyLaraSerran <85379351+MaolyLaraSerrano@users.noreply.github.com>
2021-09-14 03:07:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var nome = "Maoly Lara Serrano"
+var notaDoPrimeiroBimestre = 9.4355
+var notaDoSegundoBimestre = 7.234
+var notaDoTerceiroBimestre = 4.234
+var notaDoQuartoBimestre = 2.183
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+
+var notaFixada = notaFinal.toFixed(1)
+
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+//isso é um comecomentário 
+
+//Revisão 
+//variáveis,strings, console.log, toFixed, operações matemática, concatenação 
